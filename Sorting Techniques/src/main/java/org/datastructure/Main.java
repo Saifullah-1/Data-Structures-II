844690c69ded014de3648af7bd94afaf0120f2fb
Saifullah-1 <siafmosad@gmail.com>
2024-03-14 11:02:17 +0200

    Initial

--- Sorting Techniques/src/main/java/org/datastructure/Main.java
+++ Sorting Techniques/src/main/java/org/datastructure/Main.java
@@ -1,17 +1,95 @@
 package org.datastructure;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void printList(List<List<Integer>> list) {
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(System.in);
         while(true) {
             // Ask user for file path
             System.out.print("Enter file Path: ");
             String filePath = sc.nextLine();
-            SortArray sortArray = new SortArray();
+            File file = new File(filePath);
+            if (!file.exists()) {
+                System.out.println("File not found!");
+                continue;
+            }
+            SortArray sortArray = new SortArray(file);
             // Print the menu [1] Simple Sort [2] Efficient Sort [3] Non-Comparison Sort [4] exit
-            // Ask user if he wants intermediate arrays
+            System.out.println("Please select sort type: [1] Simple Sort [2] Efficient Sort [3] Non-Comparison Sort");
+            int sortType = sc.nextInt();
+            char ch;
+            List<List<Integer>> list;
+            switch (sortType) {
+                case 1:
+                    // Ask user if he wants intermediate arrays
+                    System.out.print("Do you want intermediate arrays? Y/N");
+                    ch = sc.next().charAt(0);
+                    switch (ch) {
+                        case 'Y': case 'y':
+                            list = sortArray.simpleSort(true);
+                            // print list
+                            printList(list);
+                            break;
+                        case 'N': case 'n':
+                            list = sortArray.simpleSort(false);
+                            // print list
+                            printList(list);
+                            break;
+                        default:
+                            // TODO
+                            break;
+                    }
+                    break;
+                case 2:
+                    // Ask user if he wants intermediate arrays
+                    System.out.print("Do you want intermediate arrays? Y/N");
+                    ch = sc.next().charAt(0);
+                    switch (ch) {
+                        case 'Y': case 'y':
+                            list = sortArray.efficientSort(true);
+                            // print list
+                            printList(list);
+                            break;
+                        case 'N': case 'n':
+                            list = sortArray.efficientSort(false);
+                            // print list
+                            printList(list);
+                            break;
+                        default:
+                            // TODO
+                            break;
+                    }
+                    break;
+                case 3:
+                    // Ask user if he wants intermediate arrays
+                    System.out.print("Do you want intermediate arrays? Y/N");
+                    ch = sc.next().charAt(0);
+                    switch (ch) {
+                        case 'Y': case 'y':
+                            list = sortArray.nonComparisonSort(true);
+                            // print list
+                            printList(list);
+                            break;
+                        case 'N': case 'n':
+                            list = sortArray.nonComparisonSort(false);
+                            // print list
+                            printList(list);
+                            break;
+                        default:
+                            // TODO
+                            break;
+                    }
+                    break;
+            }
         }
     }
 }
--- Sorting Techniques/src/main/java/org/datastructure/SortArray.java
+++ Sorting Techniques/src/main/java/org/datastructure/SortArray.java
@@ -1,25 +1,32 @@
 package org.datastructure;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SortArray {
-    private List<Integer> list;
+    private List<Integer> list = new ArrayList<>();
 
-    public SortArray() {}
-    public SortArray(File file) {
+    public SortArray(File file) throws FileNotFoundException {
+        Scanner fileScanner = new Scanner(file);
+        String []in = fileScanner.nextLine().split(", ");
+        for (int i = 0; i < in.length; ++i) {
+            int num = Integer.parseInt(in[i]);
+            list.add(num);
+        }
+    }
+
+    public List<List<Integer>> simpleSort(boolean intermediate) {
 
     }
 
-    public List<Integer> simpleSort(boolean intermediate) {
+    public List<List<Integer>> efficientSort(boolean intermediate) {
 
     }
 
-    public List<Integer> efficientSort(boolean intermediate) {
-
-    }
-
-    public List<Integer> nonComparisonSort(boolean intermediate) {
+    public List<List<Integer>> nonComparisonSort(boolean intermediate) {
 
     }
 }
